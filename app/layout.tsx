b6473f2db55d833229a4a8a700ffae937c1eee3e
koshambiB <bardhankoshambi2005@gmail.com>
2024-09-22 12:01:27 +0530

    chore:new updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,13 +5,12 @@ import Navbar from "@/components/NavBar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Koshu",
+  title: "Koshambi Bardhan",
 };
 
-export default function Layout({children}) {
+export default function Layout({children}:any) {
   return (
     <html lang="en">
-      <head><script src="https://kit.fontawesome.com/58256cc3e5.js" crossOrigin="anonymous"></script></head>
       <body className="bg-gray-300 box-border overflow-y-auto"> 
         {children}
         </body>
