e9ecdf9a45239d324acec1fde35ad2c710cbf964
koshambiB <bardhankoshambi2005@gmail.com>
2024-10-03 19:41:41 +0530

    feat: about section updated

--- app/page.tsx
+++ app/page.tsx
@@ -7,14 +7,16 @@ import Header from "@/components/Header";
 export default function Home() {
   return (
     <div
-      className=" m-20 overflow-y-auto bg-background  shadow-gray-900 shadow-xl"
+      className="m-5  md:m-10 overflow-y-auto bg-background shadow-gray-900 shadow-xl"
       style={{ boxShadow: "10px 0px 25px rgba(0, 0, 0, 0.7)" }}
     >
       <Header />
-      <div className="box-border mx-32 mt-8 ">
-        <About />
-        <Skills />
-        <Projects />
+      <div className="flex flex-col items-center mt-8">
+        <div className="w-full max-w-4xl px-4 sm:px-6  md:px-0">
+          <About />
+          <Skills />
+          <Projects />
+        </div>
       </div>
     </div>
   );
--- pages/About.tsx
+++ pages/About.tsx
@@ -5,18 +5,17 @@ const About = () => {
     <div className="relative flex justify-center font-gowun items-center pt-8 mb-12">
       <div className="text-center ">
         <p className="text-4xl   text-foreground">Hi, I am Koshambi Bardhan</p>
-        <p className="text-bluecolor text-3xl pt-5 pb-8">I am a Full-Stack Developer</p>
+        <p className="text-bluecolor text-3xl pt-5 pb-8">I am an open-source developer</p>
         <p className="text-lg text-gray-500 pb-10">
-        My current expertise lies heavily in front-end technologies, where I’m skilled in crafting engaging, user-friendly interfaces that prioritize both aesthetics and functionality.
-        While I’m still in the early stages of mastering backend development, I’m actively expanding my knowledge in this area, learning the ins and outs of databases, server-side frameworks, and API integration.
-        In addition to my development skills, I have a deep interest in cybersecurity and web exploitation, which keeps me aware of the security implications of my work.
-        Whether it’s meeting deadlines or working collaboratively with a team, these skills allow me to manage complex projects efficiently. My attention to detail, 
-        and problem-solving abilities translate directly into my approach as a developer, ensuring that every project is properly planned and executed.
+        I specialize in front-end technologies, crafting engaging and user-friendly interfaces. 
+        While I’m still mastering back-end development, I'm actively expanding my knowledge in that area. 
+        I also have a keen interest in cybersecurity and web exploitation, dedicating time to learn best practices in the field. 
+        My skills enable me to manage complex projects effectively, whether meeting deadlines or collaborating with a team. 
+        With a strong attention to detail and problem-solving abilities, I ensure that every project is well-planned and executed.
         </p>
         <button className='border border-gray-400 rounded-xl py-3 px-5 text-gray-400 hover:border-white hover:text-white'>Resume</button>
       </div>
     </div>
   );
 };
-
 export default About;
